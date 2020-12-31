a5e8a7cc7b4a8583e64320df3657ff508f43d16c
bol0gna <75225753+bol0gna@users.noreply.github.com>
2020-12-31 02:12:28 -0500

    Chapter 4: windows portability

--- prompt.c
+++ prompt.c
@@ -1,12 +1,31 @@
 /*
- * lisB (lisp by bol0gna)
- * version 0.0.2
+ * lisB (lisp by bol0gna) REPL shell
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
+/* if compiling on windows, replace editline functions with fakes */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+void add_history(char* unused) {}
+
+/* if compiling on unix, include editline functions */
+#else
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
 
