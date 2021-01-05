f1c8fe8c08f06a04f61bb03ef858fae90df41b92
bol0gna <75225753+bol0gna@users.noreply.github.com>
2021-01-05 15:44:47 -0500

    lesson 2 step 5

--- cforjava/functions.c
+++ cforjava/functions.c
@@ -11,14 +11,31 @@ double toKelvin(double degF) {
 	return (degF + 459.67) * (5.0 / 9.0);
 }
 
+void scanValues() {
+	int i;
+	float f;
+	char s[256];
+
+	printf("integer: ");
+	scanf("%d", &i);
+	printf("floating point value: ");
+	scanf("%f", &f);
+	printf("string 255 chars or less: ");
+	scanf("%s", s);
+
+	printf("integer was %d\nfloating point was %f\nstring was %s\n", i, f, s);
+}
+
 int main(int argc, char* argv[]) {
 	printf("print this many stars: ");
 	int stars;
 	scanf("%d", &stars);
 	printStars(stars);
 
-	printf("\nconvert fahrenheit to kelvin: ");
+	printf("convert fahrenheit to kelvin: ");
 	double fahrenheit;
 	scanf("%lf", &fahrenheit);
-	printf("%.2lf", toKelvin(fahrenheit));
+	printf("%.2lf\n", toKelvin(fahrenheit));
+
+	scanValues();
 }
