1785f22903545607c222df17469a7ffbf1be57f4
bol0gna <75225753+bol0gna@users.noreply.github.com>
2021-01-05 15:05:53 -0500

    lesson 2 step 2

--- cforjava/kelvin.c
+++ cforjava/kelvin.c
@@ -5,6 +5,14 @@ int main(int argc, char* argv[]) {
 	double degF;
 	scanf("%lf", &degF);
 
+	if (degF <= 32) {
+		puts("That's freezing!");
+	} else if (degF >= 212) {
+		puts("That's boiling!");
+	} else {
+		puts("That's nice.");
+	}
+
 	double kelvin = (degF + 459.67) * (5.0 / 9.0);
 
 	printf("%.2lf degrees F is %.2lf K\n", degF, kelvin);
