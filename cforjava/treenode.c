423b479f718e522a031bcfe7f02f719c497641c7
bol0gna <75225753+bol0gna@users.noreply.github.com>
2021-01-08 11:39:52 -0500

    lesson 4 step 17

--- cforjava/treenode.c
+++ cforjava/treenode.c
@@ -6,15 +6,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct node* Node;
 struct node {
 	int info;
-	struct node* left;
-	struct node* right;
+	Node left;
+	Node right;
 };
 
 /* allocate and initialize new node with an int value to store */
-struct node* new_node(int val) {
-	struct node* this = (struct node*)malloc(sizeof(struct node));
+Node new_Node(int val) {
+	Node this = (Node)malloc(sizeof(struct node));
 	if (this == NULL) {
 		return NULL; // out of memory
 	}
@@ -27,91 +28,92 @@ struct node* new_node(int val) {
 }
 
 /* recursively free this node and all children */
-void free_node(struct node* this) {
+void free_Node(Node this) {
 	if (this == NULL) return;
 	if (this->left == NULL && this->right == NULL) {
 		free(this);
 		return;
 	}
-	free_node(this->left);
-	free_node(this->right);
+	free_Node(this->left);
+	free_Node(this->right);
 }
 
 /* print this node only */
-void node_print(struct node* this) {
+void Node_print(Node this) {
 	if (this != NULL) {
 		printf("Node[%d]", this->info);
 	}
 }
 
 /* recursively print this node and all children, in-order traversal */
-void node_treeprint(struct node* this) {
+void Node_treeprint(Node this) {
 	if (this != NULL) {
-		node_treeprint(this->left);
-		node_print(this);
-		node_treeprint(this->right);
+		Node_treeprint(this->left);
+		Node_print(this);
+		Node_treeprint(this->right);
 	}
 }
 
 /* add a value to the binary search tree rooted at this node */
-struct node* node_add(struct node* this, int val) {
+Node Node_add(Node this, int val) {
 	if (this == NULL) {
-		this = new_node(val);
+		this = new_Node(val);
 	} else if (val < this->info) {
-		this->left = node_add(this->left, val);
+		this->left = Node_add(this->left, val);
 	} else if (val > this->info) {
-		this->right = node_add(this->right, val);
+		this->right = Node_add(this->right, val);
 	} // no action taken if val is already in tree
 	return this;
 }
 
 /* lookup a value using binary search tree return whether value is in tree */
-int node_lookup(struct node* this, int val) {
+int Node_lookup(Node this, int val) {
 	if (this == NULL) {
 		return 0;
 	} else if (val < this->info) {
-		return node_lookup(this->left, val);
+		return Node_lookup(this->left, val);
 	} else if (val > this->info) {
-		return node_lookup(this->right, val);
+		return Node_lookup(this->right, val);
 	} else {
 		return 1;
 	}
 }
 
 /* apply void function(int) referenced by fp to each value in sorted order */
-void node_apply(struct node* this, void (*fp)(int)) {
+void Node_apply(Node this, void (*fp)(int)) {
 	if (this != NULL) {
-		node_apply(this->left, fp);
+		Node_apply(this->left, fp);
 		(*fp)(this->info);
-		node_apply(this->right, fp);
+		Node_apply(this->right, fp);
 	}
 }
 
+typedef struct tree* Tree;
 struct tree {
-	struct node* root;
+	Node root;
 };
 
-struct tree* new_tree() {
-	struct tree* this = (struct tree*)malloc(sizeof(struct tree));
+Tree new_Tree() {
+	Tree this = (Tree)malloc(sizeof(struct tree));
 	if (this == NULL) return this;
 	this->root = NULL;
 	return this;
 }
 
-void tree_print(struct tree* this) {
-	node_treeprint(this->root);
+void Tree_print(Tree this) {
+	Node_treeprint(this->root);
 }
 
-void tree_add(struct tree* this, int val) {
-	this->root = node_add(this->root, val);
+void Tree_add(Tree this, int val) {
+	this->root = Node_add(this->root, val);
 }
 
-int tree_lookup(struct tree* this, int val) {
-	return node_lookup(this->root, val);
+int Tree_lookup(Tree this, int val) {
+	return Node_lookup(this->root, val);
 }
 
-void tree_apply(struct tree* this, void (*fp)(int)) {
-	node_apply(this->root, fp);
+void Tree_apply(Tree this, void (*fp)(int)) {
+	Node_apply(this->root, fp);
 }
 
 void callbackTest(int val) {
@@ -119,39 +121,39 @@ void callbackTest(int val) {
 }
 
 int main(int argc, char* argv[]) {
-	struct node* a = new_node(123);
-	struct node* b = new_node(7);
-	struct node* c = new_node(1);
+	Node a = new_Node(123);
+	Node b = new_Node(7);
+	Node c = new_Node(1);
 
-	node_print(a);
-	node_print(b);
-	node_print(c);
+	Node_print(a);
+	Node_print(b);
+	Node_print(c);
 	printf("\n");
 
 	b->right = a;
 	c->right = b;
-	node_treeprint(c);
+	Node_treeprint(c);
 	printf("\n");
 
-	struct node* d = node_add(NULL, 5);
-	node_add(d, 6);
-	node_add(d, 2);
-	node_add(d, 1);
-	node_add(d, 5);
+	Node d = Node_add(NULL, 5);
+	Node_add(d, 6);
+	Node_add(d, 2);
+	Node_add(d, 1);
+	Node_add(d, 5);
 
-	node_treeprint(d);
+	Node_treeprint(d);
 	printf("\n");
 
-	struct tree* h = new_tree();
-	tree_add(h, 1);
-	tree_add(h, 2);
-	tree_add(h, 4);
-	tree_add(h, 3);
-	tree_print(h);
+	Tree h = new_Tree();
+	Tree_add(h, 1);
+	Tree_add(h, 2);
+	Tree_add(h, 4);
+	Tree_add(h, 3);
+	Tree_print(h);
 	printf("\n");
 
-	printf("3 in tree: %d\n5 in tree: %d\n", tree_lookup(h, 3), tree_lookup(h, 5));
+	printf("3 in tree: %d\n5 in tree: %d\n", Tree_lookup(h, 3), Tree_lookup(h, 5));
 
-	tree_apply(h, &callbackTest);
+	Tree_apply(h, &callbackTest);
 	printf("\n");
 }
