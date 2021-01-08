a701de4de65b8efd18b89d8b0f8c2769cb241ffc
bol0gna <75225753+bol0gna@users.noreply.github.com>
2021-01-08 11:25:14 -0500

    lesson 4 step 16

--- cforjava/treenode.c
+++ cforjava/treenode.c
@@ -53,6 +53,7 @@ void node_treeprint(struct node* this) {
 	}
 }
 
+/* add a value to the binary search tree rooted at this node */
 struct node* node_add(struct node* this, int val) {
 	if (this == NULL) {
 		this = new_node(val);
@@ -62,6 +63,28 @@ struct node* node_add(struct node* this, int val) {
 		this->right = node_add(this->right, val);
 	} // no action taken if val is already in tree
 	return this;
+}
+
+/* lookup a value using binary search tree return whether value is in tree */
+int node_lookup(struct node* this, int val) {
+	if (this == NULL) {
+		return 0;
+	} else if (val < this->info) {
+		return node_lookup(this->left, val);
+	} else if (val > this->info) {
+		return node_lookup(this->right, val);
+	} else {
+		return 1;
+	}
+}
+
+/* apply void function(int) referenced by fp to each value in sorted order */
+void node_apply(struct node* this, void (*fp)(int)) {
+	if (this != NULL) {
+		node_apply(this->left, fp);
+		(*fp)(this->info);
+		node_apply(this->right, fp);
+	}
 }
 
 struct tree {
@@ -83,20 +106,16 @@ void tree_add(struct tree* this, int val) {
 	this->root = node_add(this->root, val);
 }
 
-int node_lookup(struct node* this, int val) {
-	if (this == NULL) {
-		return 0;
-	} else if (val < this->info) {
-		return node_lookup(this->left, val);
-	} else if (val > this->info) {
-		return node_lookup(this->right, val);
-	} else {
-		return 1;
-	}
-}
-
 int tree_lookup(struct tree* this, int val) {
 	return node_lookup(this->root, val);
+}
+
+void tree_apply(struct tree* this, void (*fp)(int)) {
+	node_apply(this->root, fp);
+}
+
+void callbackTest(int val) {
+	printf("%d ", val);
 }
 
 int main(int argc, char* argv[]) {
@@ -132,4 +151,7 @@ int main(int argc, char* argv[]) {
 	printf("\n");
 
 	printf("3 in tree: %d\n5 in tree: %d\n", tree_lookup(h, 3), tree_lookup(h, 5));
+
+	tree_apply(h, &callbackTest);
+	printf("\n");
 }
