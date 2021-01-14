af64b59659dc3cacdb36ac09bd625ec40fa46431
bol0gna <75225753+bol0gna@users.noreply.github.com>
2021-01-13 22:50:44 -0500

    starting over from ch.4

--- buildyourownlisp/prompt.c
+++ buildyourownlisp/prompt.c
@@ -1,36 +1,15 @@
 /*
- * lisB (lisp by bol0gna) REPL shell
+ * lisB (lisp by bol0gna)
  */
 
 #include <stdlib.h>
 #include <stdio.h>
-
-/* if compiling on windows, replace editline functions with fakes */
-#ifdef _WIN32
-#include <string.h>
-
-static char buffer[2048];
-
-char* readline(char* prompt) {
-	fputs(prompt, stdout);
-	fgets(buffer, 2048, stdin);
-	char* cpy = malloc(strlen(buffer)+1);
-	strcpy(cpy, buffer);
-	cpy[strlen(cpy)-1] = '\0';
-	return cpy;
-}
-
-void add_history(char* unused) {}
-
-/* if compiling on unix, include editline functions */
-#else
 #include <editline/readline.h>
-#endif
 
 int main(int argc, char** argv) {
 
 	/* version and exit info */
-	puts("lisB version 0.0.2, Ctrl+c to exit\n");
+	puts("lisB version 0.0.1, Ctrl+c to exit\n");
 
 	while(1) {
 
